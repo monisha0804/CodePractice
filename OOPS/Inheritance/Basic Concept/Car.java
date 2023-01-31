85a51843303d0ed8b2ac417152bc55daf1eb0577
MONISHA R <92911759+monisha0804@users.noreply.github.com>
2023-01-31 18:50:56 +0530

    Create Car.java

---
+++ OOPS/Inheritance/Basic Concept/Car.java
@@ -0,0 +1,13 @@
+public class Car extends Vehicle{
+	
+	public void car() {
+		System.out.println("hello car");
+	}
+	
+	public void vehicle1() {
+		System.out.println("hi vehicle");
+	}
+	
+	{super.vehicle();
+	}
+}
