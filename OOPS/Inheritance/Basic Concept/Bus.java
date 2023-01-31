f12b1a3dab200b3951b64e8c059484692f461e06
MONISHA R <92911759+monisha0804@users.noreply.github.com>
2023-01-31 18:51:16 +0530

    Create Bus.java

---
+++ OOPS/Inheritance/Basic Concept/Bus.java
@@ -0,0 +1,14 @@
+public class Bus extends Car {
+	
+	void horn() {
+		System.out.println("bus is horning");
+	}
+	
+	//super method is used for immediate parents
+	{
+super.car();
+	}
+	{
+super.vehicle1();	
+}
+}
